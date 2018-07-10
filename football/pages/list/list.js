c01c354e53dea7c45414db6edcd322c5476e54d7
86d_100004009520 <lcg_car@163.com>
2018-07-10 23:12:04 +0800

    地址列表

---
+++ football/pages/list/list.js
@@ -0,0 +1,28 @@
+// pages/list/list.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+		list:[
+			{
+				icon:"https://dawn20999.github.io/onlineImages/givelove/loveiocn@2x.png",
+				title:"青岛胜地（北京-立水桥）",
+			},
+			{
+				icon:"https://dawn20999.github.io/onlineImages/givelove/loveiocn@2x.png",
+				title:"青岛胜地（北京-立水桥）",
+			},
+			{
+				icon:"https://dawn20999.github.io/onlineImages/givelove/loveiocn@2x.png",
+				title:"青岛胜地（北京-立水桥）",
+			},
+		]
+    },
+
+    itemPage:function(e){
+		console.log(e)
+	
+	}
+});
